fbd2594641068992ef21ed300e334f982bcd2cfa
Bardin08 <vbardin810@gmail.com>
2024-05-11 03:15:20 +0300

    Add handling for `ping` & non-create repo events

--- EduAutomation/Rest/GitHub/GitHubEndpoints.cs
+++ EduAutomation/Rest/GitHub/GitHubEndpoints.cs
@@ -1,20 +1,30 @@
-﻿using EduAutomation.Application.GitHub;
-using EduAutomation.Application.GitHub.Services;
-using EduAutomation.Application.Telegram;
-using EduAutomation.Application.Trello;
+﻿using EduAutomation.Application.GitHub.Services;
 using EduAutomation.Rest.GitHub.Mappers;
 using EduAutomation.Rest.GitHub.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EduAutomation.Rest.GitHub;
 
-public class GitHubEndpoints
+public static class GitHubEndpoints
 {
     public static async ValueTask<IResult> RepositoryCreatedWebhook(
         RepositoryEventPayload payload,
+        HttpRequest request,
         CancellationToken cancellationToken,
         [FromServices] IGitHubWebHookService webHookService)
     {
+        if (request.Headers.TryGetValue("X-GitHub-Event", out var header) && header == "ping")
+        {
+            return Results.Ok("pong");
+        }
+
+        var action = payload.Action;
+        if (action is not "created")
+        {
+            // don't process any repo related action except repo creation event.
+            return Results.NoContent();
+        }
+
         await webHookService.HandleRepoCreated(payload.ToDomainModel(), true, true);
         return Results.Ok();
     }
